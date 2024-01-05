3c1ce9c14b3e603c3fa6ef4f55344833f01e1706
Mattia Campari <155652768+ProjectGoofball@users.noreply.github.com>
2024-01-05 13:12:06 +0100

    we are BACK baby

--- trk2obj.py
+++ trk2obj.py
@@ -1,1 +1,90 @@
-UNDER MAINTANANCE
+import time
+def IlIIIIIIlIllIII(lIIIIlIlllllII, IllllllllIlII):
+    for llIlIIIIIIlIIII in IllllllllIlII.wverts:
+        IlIllIllIlIllIIII = llIlIIIIIIlIIII
+        lIIIIlIlllllII.write(lIIIIlIlllllII"v {wp.x} {wp.y} {-IlIllIllIlIllIIII.z}\n")
+def lIllIIIlII(lIIIIlIlllllII, IllllllllIlII, IllllIIIIIlIl):
+    lIIllIIIllIIlIIlIIlI = materials[IllllllllIlII.IIIIlIIIl]
+    llllIIlIIIIlIIIIll, lIlIlIIl = VEC3(), VEC3()
+    llllIlllIIlIIIIIl, IlllllIllllIIllI = 0, 0
+    IIIlIIIllllIl = IllllllllIlII.planes[0]
+    lIllllIIIllI = IllllllllIlII.indices[0]
+    while lIllllIIIllI < IllllllllIlII.indices[-1]:
+        llllIIlIIIIlIIIIll = (IIIlIIIllllIl.normal % IIIlIIIllllIl.normal.findAnyPerpendVector()).normalized()
+        lIlIlIIl = IIIlIIIllllIl.normal % llllIIlIIIIlIIIIll
+        llllIIlIIIIlIIIIll *= lIIllIIIllIIlIIlIIlI.scaleX * IllllllllIlII.texscale.x
+        lIlIlIIl *= lIIllIIIllIIlIIlIIlI.scaleY * IllllllllIlII.texscale.y
+        IlllllIllllIIllI = (IllllllllIlII.textureCenterShift * llllIIlIIIIlIIIIll) - 0.5
+        llllIlllIIlIIIIIl = (IllllllllIlII.textureCenterShift * lIlIlIIl) - 0.5
+        llIlIIIII = lIllllIIIllI
+        lIllllIIIllI += 1
+        for IlIIIllIlIlllIIIl in range(llIlIIIII):
+            lllIlllIIIIIIIIl = (((IllllllllIlII.verts[lIllllIIIllI] * llllIIlIIIIlIIIIll) - IlllllIllllIIllI))
+            lIIlllIIIlllIIIlIlI = -(((IllllllllIlII.verts[lIllllIIIllI] * lIlIlIIl) - llllIlllIIlIIIIIl))
+            lIIIIlIlllllII.write(lIIIIlIlllllII"vt {lllIlllIIIIIIIIl} {lIIlllIIIlllIIIlIlI}\n")
+            IllllIIIIIlIl += 1
+            lIllllIIIllI += 1
+        IIIlIIIllllIl += 1
+def llIIIIIIIllIlIIll(lIIIIlIlllllII, IllllllllIlII, llIIIlIlIlII, llIlIIllII):
+    lIllllIIIllI = IllllllllIlII.indices[0]
+    while lIllllIIIllI < IllllllllIlII.indices[-1]:
+        llIlIIIII = lIllllIIIllI
+        lIllllIIIllI += 1
+        lIIIIlIlllllII.write("f ")
+        for IlIIIllIlIlllIIIl in range(llIlIIIII):
+            lIIIIlIlllllII.write(lIIIIlIlllllII"{IlIIIllIlIlllIIIlnd + llIIIlIlIlII}/{llIlIIllII} ")
+            lIllllIIIllI += 1
+            llIlIIllII += 1
+        lIIIIlIlllllII.write("\n")
+    llIIIlIlIlII += len(IllllllllIlII.wverts)
+def IIIllllIlllllIIl():
+    IlllIllIlllIIlI = time.time()
+    IIIllIIIlI = time.localtime(IlllIllIlllIIlI)
+    lllIIllIl = time.strftime("%M_%S", IIIllIIIlI)
+    IllllIIllllIIIl = lIIIIlIlllllII"goofball_scene_{tIlIIIllIlIlllIIIlmebulIIIIlIlllllII}.mtl"
+    llIllIIlllllIIII = lIIIIlIlllllII"goofball_scene_{tIlIIIllIlIlllIIIlmebulIIIIlIlllllII}.obj"
+    lIIIIlIlllllII = open(IllllIIllllIIIl, "wb")
+    for IlIIIllIlIlllIIIl in range(len(materials[IlIIIllIlIlllIIIl].texturefilename)):
+        lIIIIlIlllllII.write(lIIIIlIlllllII"newmtl tex{IlIIIllIlIlllIIIl}\n")
+        lIIIIlIlllllII.write("Kd 1.0 1.0 1.0\n")
+        lIIIIlIlllllII.write("Ka 0.5 0.5 0.5\n")
+        lIIIIlIlllllII.write(lIIIIlIlllllII"map_Kd {materials[i].texlllIlllIIIIIIIIlrelIIIIlIlllllIIIlIIIllIlIlllIIIlleIlIllllIII}\n")
+    lIIIIlIlllllII.close()
+    lIIIIlIlllllII = open(llIllIIlllllIIII, "wb")
+    lIIIIlIlllllII.write("# Wavefront OBJ file exported by Goofball Goals\n\n")
+    llIIIlIlIlII = 1
+    IllllIIIIIlIl = 1
+    llIIIllllll = 0
+    IlIllllIII = "b"
+    IIIllIIlllI = AMESH()
+    IIIllIIlllI.createSphere(3)
+    for IIllIIIllIIIlIIlIlII in objs:
+        if IIllIIIllIIIlIIlIlII.alive() and IIllIIIllIIIlIIlIlII.triggermanusindex == 0:
+            lllIlllIlIII = IllllIIIIIlIl
+            lIIIIlIlllllII.write(lIIIIlIlllllII"#\n# obj: {IlIllllIII}{IlIIIllIlIlllIIIlndex:04d}\n#\n\n")
+            for IIIllIllllIlIlIIl in range(len(IIllIIIllIIIlIIlIlII.ameshes)):
+                IlIIIIIIlIllIII(lIIIIlIlllllII, IIllIIIllIIIlIIlIlII.ameshes[IIIllIllllIlIlIIl])
+            for IlllIlIlIlIll in range(len(IIllIIIllIIIlIIlIlII.ellipdoids)):
+                IIIllIIlllI.transform(IIllIIIllIIIlIIlIlII.ellipdoids[IlllIlIlIlIll].wtrans)
+                IlIIIIIIlIllIII(lIIIIlIlllllII, IIIllIIlllI)
+            for IIIllIllllIlIlIIl in range(len(IIllIIIllIIIlIIlIlII.ameshes)):
+                lIllIIIlII(lIIIIlIlllllII, IIllIIIllIIIlIIlIlII.ameshes[IIIllIllllIlIlIIl], IllllIIIIIlIl)
+            for IlllIlIlIlIll in range(len(IIllIIIllIIIlIIlIlII.ellipdoids)):
+                IIIllIIlllI.transform(IIllIIIllIIIlIIlIlII.ellipdoids[IlllIlIlIlIll].wtrans)
+                lIllIIIlII(lIIIIlIlllllII, IIIllIIlllI, IllllIIIIIlIl)
+            lIIIIlIlllllII.write(lIIIIlIlllllII"\ng {IlIllllIII}{IlIIIllIlIlllIIIlndex:04d}\ns off\n")
+            IIIIlIIIl = 0
+            if len(IIllIIIllIIIlIIlIlII.ameshes) > 0:
+                IIIIlIIIl = IIllIIIllIIIlIIlIlII.ameshes[0].IIIIlIIIl
+            if len(IIllIIIllIIIlIIlIlII.ellipdoids) > 0:
+                IIIIlIIIl = IIllIIIllIIIlIIlIlII.ellipdoids[0].IIIIlIIIl
+            lIIIIlIlllllII.write("mtllib tt_scene.mtl\n")
+            lIIIIlIlllllII.write(lIIIIlIlllllII"usemtl tex{texlllIlllIIIIIIIIlre}\n")
+            IllllIIIIIlIl = lllIlllIlIII
+            for IIIllIllllIlIlIIl in range(len(IIllIIIllIIIlIIlIlII.ameshes)):
+                IllllllllIlII = IIllIIIllIIIlIIlIlII.ameshes[IIIllIllllIlIlIIl]
+                llIIIIIIIllIlIIll(lIIIIlIlllllII, IllllllllIlII, llIIIlIlIlII, IllllIIIIIlIl)
+            for IlllIlIlIlIll in range(len(IIllIIIllIIIlIIlIlII.ellipdoids)):
+                llIIIIIIIllIlIIll(lIIIIlIlllllII, IIIllIIlllI, llIIIlIlIlII, IllllIIIIIlIl)
+        llIIIllllll += 1
+    lIIIIlIlllllII.close()
